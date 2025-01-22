aa01c2d30010cba543e6bda193fac5730b41f50e
Nishita Panchal <nishakp3005@gmail.com>
2025-01-22 19:25:57 +0530

    retrieved images

--- src/app/(authorized)/home/page.tsx
+++ src/app/(authorized)/home/page.tsx
@@ -1,15 +1,31 @@
 import { auth } from "@/lib/auth";
+import { Pool } from '@neondatabase/serverless';
 
 export const runtime = "edge";
 
 export default async function HomePage() {
   const session = await auth();
-
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const userResult = await pool.query(
+    'SELECT id FROM users WHERE email = $1',
+    [session?.user?.email]
+  );
+  const userId = userResult.rows[0]?.id;
+  console.log(userId);
+  // Second query - get images
+  const imagesResult = await pool.query(
+    'SELECT public_url FROM images WHERE owner = $1',
+    [userId]
+  );
+  const images = imagesResult.rows;
+  console.log(images);
+  
+  await pool.end();
   return (
     <main>
       <div className="mx-auto max-w-7xl">
         <h1 className="mb-6 text-4xl font-bold text-[#eeeeee]">
-          Welcome back, {session?.user?.name}!
+          Welcome backs, {session?.user?.name}!
         </h1>
       </div>
     </main>
